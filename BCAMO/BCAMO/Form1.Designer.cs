a7ae7e4dad8494e1d834d0ebf2398367585726cd
rhjason <rhjason@bu.edu>
2014-11-24 00:52:12 -0500

    Conversion from Decimal to Base N
    ENGINE
    -Created class for base conversion that is not basic
    -Detects if the base is basic (2, 8, 10, 16)
    -Has basic length check and restriction (Int32)
    -Working Base N to decimal (unsigned) conversion
    
    GUI
    -The switching image is implemented (self drawn)

--- BCAMO/BCAMO/Form1.Designer.cs
+++ BCAMO/BCAMO/Form1.Designer.cs
@@ -45,8 +45,10 @@
             this.otherToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.convertbutton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.convertfrom.SuspendLayout();
             this.convertto.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // convertfrom
@@ -54,7 +56,7 @@
             this.convertfrom.Dock = System.Windows.Forms.DockStyle.None;
             this.convertfrom.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.convertFromToolStripMenuItem});
-            this.convertfrom.Location = new System.Drawing.Point(9, 100);
+            this.convertfrom.Location = new System.Drawing.Point(9, 32);
             this.convertfrom.Name = "convertfrom";
             this.convertfrom.Size = new System.Drawing.Size(109, 24);
             this.convertfrom.TabIndex = 0;
@@ -75,42 +77,42 @@
             // binaryToolStripMenuItem
             // 
             this.binaryToolStripMenuItem.Name = "binaryToolStripMenuItem";
-            this.binaryToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.binaryToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.binaryToolStripMenuItem.Text = "Binary";
             this.binaryToolStripMenuItem.Click += new System.EventHandler(this.binaryToolStripMenuItem_Click);
             // 
             // octalToolStripMenuItem
             // 
             this.octalToolStripMenuItem.Name = "octalToolStripMenuItem";
-            this.octalToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.octalToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.octalToolStripMenuItem.Text = "Octal";
             this.octalToolStripMenuItem.Click += new System.EventHandler(this.octalToolStripMenuItem_Click);
             // 
             // decimalToolStripMenuItem
             // 
             this.decimalToolStripMenuItem.Name = "decimalToolStripMenuItem";
-            this.decimalToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.decimalToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.decimalToolStripMenuItem.Text = "Decimal";
             this.decimalToolStripMenuItem.Click += new System.EventHandler(this.decimalToolStripMenuItem_Click);
             // 
             // hexadecimalToolStripMenuItem
             // 
             this.hexadecimalToolStripMenuItem.Name = "hexadecimalToolStripMenuItem";
-            this.hexadecimalToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.hexadecimalToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.hexadecimalToolStripMenuItem.Text = "Hexadecimal";
             this.hexadecimalToolStripMenuItem.Click += new System.EventHandler(this.hexadecimalToolStripMenuItem_Click);
             // 
             // otherToolStripMenuItem
             // 
             this.otherToolStripMenuItem.Name = "otherToolStripMenuItem";
-            this.otherToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.otherToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.otherToolStripMenuItem.Text = "Other";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(110, 100);
+            this.textBox1.Location = new System.Drawing.Point(121, 32);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(292, 20);
             this.textBox1.TabIndex = 1;
             // 
             // convertto
@@ -118,9 +120,9 @@
             this.convertto.Dock = System.Windows.Forms.DockStyle.None;
             this.convertto.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.convertIntoToolStripMenuItem});
-            this.convertto.Location = new System.Drawing.Point(223, 100);
+            this.convertto.Location = new System.Drawing.Point(9, 59);
             this.convertto.Name = "convertto";
-            this.convertto.Size = new System.Drawing.Size(194, 24);
+            this.convertto.Size = new System.Drawing.Size(102, 24);
             this.convertto.TabIndex = 2;
             this.convertto.Text = "menuStrip1";
             // 
@@ -172,14 +174,14 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(327, 100);
+            this.textBox2.Location = new System.Drawing.Point(121, 59);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(292, 20);
             this.textBox2.TabIndex = 3;
             // 
             // convertbutton
             // 
-            this.convertbutton.Location = new System.Drawing.Point(447, 97);
+            this.convertbutton.Location = new System.Drawing.Point(447, 59);
             this.convertbutton.Name = "convertbutton";
             this.convertbutton.Size = new System.Drawing.Size(75, 23);
             this.convertbutton.TabIndex = 4;
@@ -187,11 +189,21 @@
             this.convertbutton.UseVisualStyleBackColor = true;
             this.convertbutton.Click += new System.EventHandler(this.convertbutton_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BCAMO.Properties.Resources.Double_Arrow__transparent_;
+            this.pictureBox1.Location = new System.Drawing.Point(419, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(22, 50);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(534, 361);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.convertbutton);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -204,6 +216,7 @@
             this.convertfrom.PerformLayout();
             this.convertto.ResumeLayout(false);
             this.convertto.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,6 +241,7 @@
         private System.Windows.Forms.ToolStripMenuItem hexadecimalToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem otherToolStripMenuItem1;
         private System.Windows.Forms.Button convertbutton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- BCAMO/BCAMO/Form1.cs
+++ BCAMO/BCAMO/Form1.cs
@@ -12,11 +12,14 @@ namespace BCAMO
 {
     public partial class Form1 : Form
     {
-        private int otets;                  //otets is the default supported ToString autoconversions (one two eight ten sixteen)
+        private int otetsin;                //otets is the default supported ToString autoconversions (one two eight ten sixteen)
+        private int otetsout;               //otets is the default supported ToString autoconversions (one two eight ten sixteen)
         private string stringinput;         //input for textbox1 (aka the input)
         private string stringoutput;        //output for textbox2 (aka output)
+        private int intermediateint;
         private string displayinput;
         private string displayoutput;
+        basetootherbase baseconvert = new basetootherbase(); 
 
         public Form1()
         {
@@ -26,65 +29,214 @@ namespace BCAMO
 
         private void binaryToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            //this.otets = 2;
+            this.otetsin = 2;
             convertFromToolStripMenuItem.Text = "From Binary : ";
+            //intermediateint = baseconvert.Tobase10(otetsin, textBox1.Text);
         }
 
         private void octalToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            //this.otets = 8;
+            //this.otetsin = 8;
             convertFromToolStripMenuItem.Text = "From Octal : ";
         }
 
         private void decimalToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            //this.otets = 10;
+            this.otetsin = 10;
             convertFromToolStripMenuItem.Text = "From Decimal : ";
         }
 
         private void hexadecimalToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            //this.otets = 16;
+            //this.otetsin = 16;
             convertFromToolStripMenuItem.Text = "From Hexadecimal : ";
         }
 
         //Toolstrip menu items for output
         private void binaryToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            this.otets = 2;
+            this.otetsout = 2;
             convertIntoToolStripMenuItem.Text = "Into Binary : ";
         }
 
         private void octalToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            this.otets = 8;
+            this.otetsout = 8;
             convertIntoToolStripMenuItem.Text = "Into Octal : ";
         }
 
         private void decimalToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            this.otets = 10;
+            this.otetsout = 10;
             convertIntoToolStripMenuItem.Text = "Into Decimal : ";
+            intermediateint = baseconvert.Tobase10(otetsin, textBox1.Text);
         }
 
         private void hexadecimalToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            this.otets = 16;
+            this.otetsout = 16;
             convertIntoToolStripMenuItem.Text = "Into Hex : ";
         }
 
         private void convertbutton_Click(object sender, EventArgs e)
         {
-            int integer = 0;
-            if (Int32.TryParse(textBox1.Text, out integer))
+            int integer;
+            if ((baseconvert.basic(otetsout)) && (otetsin == 10))
             {
-                this.stringoutput = Convert.ToString(integer, otets);
+                if (Int32.TryParse(textBox1.Text, out integer))
+                {
+                    this.stringoutput = Convert.ToString(integer, otetsout);
+                }
+                else
+                {
+                    this.stringoutput = "Error, Value entered was not an integer";
+                }
             }
-            else
+            if(otetsout == 10)
             {
-                this.stringoutput = "Error, Value entered was not an integer";
+                this.stringoutput = intermediateint.ToString();
             }
             textBox2.Text = this.stringoutput;
+        }
+    }
+
+
+    public class basetootherbase
+    {
+        private int trackbase;
+        private bool noerrorflag = true;
+        public basetootherbase()
+        {}
+        
+        public bool sizefit(int basevalue, string inputstring)
+        {
+            bool tempoutput = false;
+            switch (basevalue)
+            {
+                case 0:
+                    tempoutput = false;
+                    break;
+                case 1:
+                    tempoutput = false;
+                    break;;
+                case 2:
+                    if(inputstring.Length > 31)
+                    {
+                        tempoutput = false;
+                    }
+                    else tempoutput = true;
+                    break;
+                case 8:
+                    if (inputstring.Length > 11)
+                    {
+                        tempoutput = false;
+                    }
+                    else tempoutput = true;
+                    break;
+                case 10:
+                    if (inputstring.Length > 10)
+                    {
+                        tempoutput = false;
+                    }
+                    else tempoutput = true;
+                    break;
+                case 16:
+                    if (inputstring.Length > 8)
+                    {
+                        tempoutput = false;
+                    }
+                    else tempoutput = true;
+                    break;
+            }
+            return tempoutput;
+        }
+
+        public int Tobase10(int basevalue, string valuetoconvert)
+        {
+            int temp = basevalue;
+            int templen = valuetoconvert.Length;
+            string tempvalue = valuetoconvert;
+            int output = 0;
+            switch (temp)
+            {
+                case 2:
+                    for (int j = 0; j < templen; j++)
+                    {
+                        int tempactualvalue = templen - 1;
+                        switch (valuetoconvert[j])
+                        {
+                            case '0':
+                                break;
+                            case '1':
+                                output = output + (int)Math.Pow(2, tempactualvalue - j);
+                                break;
+                            default:
+                                noerrorflag = false;
+                                break;
+                        }
+                        /*
+                        if (tempvalue[j] == '0')
+                        {
+                            output = output + 0;
+                        }
+                        else if (tempvalue[j] == '1')
+                        {
+                            output = output + (int)Math.Pow(2, j);
+                        }
+                        else output = -1;
+                        */
+                    }
+                        break;
+            }
+            /*
+            if (basevalue == 3)
+            {
+                return output;
+            }
+            else if (basevalue == 4)
+            {
+                int templength = valuetoconvert.Length;
+                for (int i = 0; i < templength; i++)
+                {
+                    //valuetoconvert[i];
+                }
+                return output;
+            }
+            */
+            return output;
+        }
+
+        public string Tobase3(int basevalue, string valuetoconvert)
+        {
+            trackbase = basevalue;
+            if (basevalue == 3)
+            {
+                return valuetoconvert;
+            }
+            else if(basevalue == 4)
+            {
+                int templength = valuetoconvert.Length;
+                for (int i = 0; i < templength; i++ )
+                {
+                    //valuetoconvert[i];
+                }
+                    return "0";
+            }
+            return "0";
+        }
+
+        public bool basic(int basevalue)
+        {
+            if ((basevalue == 1) || (basevalue == 2) || (basevalue == 8) || (basevalue == 10) || (basevalue == 16))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public bool error()
+        {
+            return this.noerrorflag;
         }
     }
 }
